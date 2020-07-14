99cdc58127280f6be37bba120fc4f899904fcc30
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-07-14 13:24:00 -0500

    step 5

--- src/index.js
+++ src/index.js
@@ -5,19 +5,15 @@ import './index.css';
 
 import api from './api';
 import shoppingList from './shopping-list';
-
+import store from './store';
 
 const main = function () {
-  api.createItem('pears')
-    .then(res => res.json())
-    .then((newItem) => {
-      return api.getItems();
-    })
-    .then(res => res.json())
-    .then((items) => {
-      console.log(items);
-    });
-    
+  api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  }); 
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
